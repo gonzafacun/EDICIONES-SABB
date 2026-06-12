915559b9f8996fb5ccaaead774e326bb27b45526
gonzalo <gonzalofacundogutierrez.40@gmail.com>
2026-06-12 00:09:17 -0300

    cambios de seguridad

--- frontend/src/config/supabase.js
+++ frontend/src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wmtvbbczyjdaciuzquqx.supabase.co';
-const supabaseAnonKey = 'sb_publishable__Er4awY8vKiiSBX709kBNA_cJlxUrgm';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
